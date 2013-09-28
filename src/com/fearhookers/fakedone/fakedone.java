518f4104af8d2d2333789aa05a2c4939b491e5fe
FearFree <fearfree@cubetown.net>
2013-09-28 12:23:07 -0500

    Add Plugin Metrics

--- src/com/fearhookers/fakedone/fakedone.java
+++ src/com/fearhookers/fakedone/fakedone.java
@@ -1,4 +1,5 @@
 package com.fearhookers.fakedone;
+import java.io.IOException;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 /**
@@ -10,8 +11,16 @@ extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("Fake Done 1.0 - McMyAdmin Version Enabled");
+        getLogger().info("This plugin collects minimal statistic data and sends it to http://mcstats.org/plugin/FakeDone");
+        getLogger().info("You can opt out, disabling metrics for all plugins, by changing the opt-out value in /plugins/PluginMetrics/config.yml");
         Bukkit.getConsoleSender().sendMessage("Done (5.000s)! For help, type \"help\" or \"?\"");
-    }
+        try {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        } catch (IOException e) {
+        // Failed to submit the stats :-(
+        }
+    } 
     
     @Override
     public void onDisable() {
