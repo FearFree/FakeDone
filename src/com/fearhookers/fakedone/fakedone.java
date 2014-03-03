af47f41793405a894eef8d83c2ac420bee281aac
FearFree <FearFree@users.noreply.github.com>
2014-03-03 15:27:21 -0500

    Update fakedone.java

--- src/com/fearhookers/fakedone/fakedone.java
+++ src/com/fearhookers/fakedone/fakedone.java
@@ -13,7 +13,7 @@ extends JavaPlugin {
         getLogger().info("Fake Done 1.0 - McMyAdmin Version Enabled");
         getLogger().info("This plugin collects minimal statistic data and sends it to http://mcstats.org/plugin/FakeDone");
         getLogger().info("You can opt out, disabling metrics for all plugins, by changing the opt-out value in /plugins/PluginMetrics/config.yml");
-        Bukkit.getConsoleSender().sendMessage("Done (5.000s)! For help, type \"help\" or \"?\"");
+        Bukkit.getConsoleSender().sendMessage("[00:00:00] [Server thread/INFO]: Done (0.000s)! For help, type \"help\" or \"?\"");
         try {
             MetricsLite metrics = new MetricsLite(this);
             metrics.start();
